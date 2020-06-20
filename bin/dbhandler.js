e76c726bd2f3899f6006642683b4ff8aefc3031f
Eric <me@bostrot.com>
2020-06-20 13:40:47 +0200

    Removed botsupport

--- bin/dbhandler.js
+++ bin/dbhandler.js
@@ -1,19 +1,25 @@
 const Database = require('better-sqlite3');
-const db = new Database(__dirname + '/support.db', { /* verbose: console.log */ }); // debugging
+const db = new Database(__dirname + '/support.db', {
+  /* verbose: console.log */}); // debugging
 
-var dbTable = db.prepare(`CREATE TABLE IF NOT EXISTS supportees (userid TEXT PRIMARY KEY, status TEXT);`);
+const dbTable = db.prepare(
+    `CREATE TABLE IF NOT EXISTS supportees
+    (userid TEXT PRIMARY KEY, status TEXT);`);
 dbTable.run();
 
 exports.check = function(userid, callback) {
-    var searchDB = db.prepare("select * from supportees where userid = " + userid).get();
-    callback(searchDB);
-}
+  const searchDB = db.prepare('select * from supportees where userid = ' +
+        userid).get();
+  callback(searchDB);
+};
 
 exports.add = function(userid, status) {
-    var stmt = db.prepare(`INSERT or REPLACE INTO supportees VALUES ('`+userid+`', '`+status+`')`).run();
-}
+  db.prepare(`INSERT or REPLACE INTO supportees VALUES ('`+
+        userid+`', '`+status+`')`).run();
+};
 
 exports.open = function(callback) {
-    var searchDB = db.prepare("select * from supportees where status = 'open'").all();
-    callback(searchDB);
-}
+  const searchDB = db.prepare(
+      'select * from supportees where status = \'open\'').all();
+  callback(searchDB);
+};
