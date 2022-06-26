d380a5faa6c0b235bd54bff75f102300321f3dfd
Eric <me@bostrot.com>
2022-06-26 15:55:06 +0200

    Improved markdown escaping

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,16 +28,60 @@ const downloadDocumentMiddleware = async function (bot, ctx, next) {
   });
 };
 
+// strict escape
+const strictEscape = function (str) {
+  let newStr = '';
+  const chars = ['[', ']', '(', ')', '_', '*', '~', '`'];
+  for (let i = 0; i < str.length; i++) {
+    // escape special characters
+    if (chars.includes(str[i])) {
+      newStr += '\\' + str[i];
+    } else {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+};
+
 // escape special characters
 const escapeText = function (str) {
-  return str.replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;');
+  if (cache.config.parse_mode == 'HTML' || cache.config.parse_mode == 'html') {
+    return str.replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;');
+  } else {
+    // '[', ']', '(', ')', are skipped as they are usally for urls
+    // '_', '*', '~', '`', are used for actualy markdown
+    const chars = ['>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
+    let newStr = '';
+    let urlNameStarted = false;
+    let urlStarted = false;
+    for (let i = 0; i < str.length; i++) {
+      // do not escape in urls
+      if (str[i] == '[') {
+        urlStarted = true;
+      } else if (str[i] == '(') {
+        urlNameStarted = true;
+      } else if (urlStarted && str[i] == ']') {
+        urlStarted = false;
+      } else if (urlNameStarted && str[i] == ')') {
+        urlNameStarted = false;
+      }
+      // escape special characters
+      if (!urlStarted && !urlNameStarted && chars.includes(str[i])) {
+        newStr += '\\' + str[i];
+      } else {
+        newStr += str[i];
+      }
+    }
+    return newStr;
+  }
 }
 
 // handle messages to web socket
-const msg = function (id, msg, extra) {
+const msg = function (id, msg, extra = {}) {
+  msg = escapeText(msg);
   // Check web message
   if (id.toString().indexOf('WEB') > -1 && id != cache.config.staffchat_id) {
     // Web message
@@ -62,7 +106,7 @@ export {
   downloadPhotoMiddleware,
   downloadVideoMiddleware,
   downloadDocumentMiddleware,
-  escapeText,
+  strictEscape,
   msg,
   reply,
 };
