333cc9a24c99de18ed3bb7f7133275deac97acea
Employer4247 <126664661+Employer4247@users.noreply.github.com>
2023-03-01 11:21:56 +0100

    TypeError: Cannot read properties of undefined (reading 'forEach') at /commands.ts:113:50

--- src/commands.ts
+++ src/commands.ts
@@ -105,7 +105,7 @@ function closeCommand(ctx: Context) {
   if (cache.config.categories) {
     cache.config.categories.forEach((element: any, index: number) => {
       // No subgroup
-      if (cache.config.categories[index].subgroups.length > 0) {
+      if (cache.config.categories[index].subgroups == undefined) {
         if (cache.config.categories[index].group_id == ctx.chat.id) {
           groups.push(cache.config.categories[index].name);
         }
